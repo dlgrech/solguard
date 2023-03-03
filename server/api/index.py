ead2e45885cb150d9e10d1083d3b8b2c9391b548
dgrech <dlgrech91@gmail.com>
2023-03-04 06:40:48 +1100

    Testing out flask

--- server/api/index.py
+++ server/api/index.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/config')
 def config():
-    return 'Hello, Config!'
+    return jsonify(
+        test1="123",
+        test2=123
+    )
